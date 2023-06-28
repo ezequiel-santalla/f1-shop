d06e06b976c7e63d46bc5648a18be3093c2cb954
Ezequiel Santalla <ezequielasantalla@gmail.com>
2023-06-28 16:40:30 -0300

    second delivery coderhouse react

--- src/asyncmock.js
+++ src/asyncmock.js
@@ -1,12 +1,12 @@
 const products = [
-  { name: "Ferrari F1 t-shirt 2023", price: 80, id: "1", idCat: "t-shirts", img: "../img/tShirtFerrari.jpg" },
-  { name: "Mercedes F1 t-shirt 2023", price: 80, id: "2", idCat: "t-shirts", img: "../img/tShirtMercedes.jpg" },
-  { name: "Ferrari F1 jacket 2023", price: 120, id: "3", idCat: "jackets", img: "../img/jacketFerrari.jpg" },
-  { name: "Mercedes F1 jacket 2023", price: 120, id: "4", idCat: "jackets", img: "../img/jacketMercedes.jpg" },
-  { name: "Ferrari F1 hoodie 2023", price: 140, id: "5", idCat: "hoodies", img: "../img/hoodieFerrari.jpg" },
-  { name: "Mercedes F1 hoodie 2023", price: 140, id: "6", idCat: "hoodies", img: "../img/hoodieMercedes.jpg" },
-  { name: "Ferrari F1 cap 2023", price: 50, id: "7", idCat: "caps", img: "../img/capFerrari.jpg" },
-  { name: "Mercedes F1 cap 2023", price: 50, id: "8", idCat: "caps", img: "../img/capMercedes.jpg" },
+  { name: "Ferrari F1 t-shirt 2023", price: 80, id: "1", idCat: "t-shirts", description: "Show your love and support for the Scuderia Ferrari F1 team with the official Replica range. This classic tee has team and sponsor details for an authentic trackside look that you can wear anywhere from the circuit to the street.", img: "../img/tShirtFerrari.jpg" },
+  { name: "Mercedes F1 t-shirt 2023", price: 80, id: "2", idCat: "t-shirts", description: "", img: "../img/tShirtMercedes.jpg" },
+  { name: "Ferrari F1 jacket 2023", price: 120, id: "3", idCat: "jackets", description: "", img: "../img/jacketFerrari.jpg" },
+  { name: "Mercedes F1 jacket 2023", price: 120, id: "4", idCat: "jackets", description: "", img: "../img/jacketMercedes.jpg" },
+  { name: "Ferrari F1 hoodie 2023", price: 140, id: "5", idCat: "hoodies", description: "", img: "../img/hoodieFerrari.jpg" },
+  { name: "Mercedes F1 hoodie 2023", price: 140, id: "6", idCat: "hoodies", description: "", img: "../img/hoodieMercedes.jpg" },
+  { name: "Ferrari F1 cap 2023", price: 50, id: "7", idCat: "caps", description: "", img: "../img/capFerrari.jpg" },
+  { name: "Mercedes F1 cap 2023", price: 50, id: "8", idCat: "caps", description: "", img: "../img/capMercedes.jpg" },
 ]
 
 export const getProducts = () => {
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,11 +1,17 @@
-const ItemDetail = ({ name, price, id, img }) => {
+const ItemDetail = ({ name, price, img }) => {
   return (
-    <div className='text-center m-12'>
-      <h2>Name: {name} </h2>
-      <h3>Price: {price} </h3>
-      <h3>ID: {id} </h3>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum laborum dolorem culpa, ipsum, pariatur molestias aliquam, dolor consequatur magni iusto porro eum rem aspernatur magnam. Deserunt facilis alias tenetur totam.</p>
-      <img className='w-80 inline-flex' src={img} alt={name} />
+    <div className='grid grid-rows-2 grid-cols-12 gap-4 mt-12'>
+      <div className='row-span-2 col-span-6 justify-self-center w-1/2'>
+        <img className='' src={img} alt={name} />
+      </div>
+      <div className='col-span-4'>
+        <h3 className='flex text-2xl font-semibold'>{name}</h3>
+        <p className='flex text-lg text-red-600 font-medium'>${price} USD</p>
+        <p className='mt-8 mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, vero deleniti fugit dignissimos voluptate, corrupti explicabo cupiditate sint quia dolores totam suscipit possimus quidem dicta nobis quo tenetur perferendis debitis!</p>
+      </div>
+      <div className='col-span-4 bg-gray-100'>
+
+      </div>
     </div>
   )
 }
